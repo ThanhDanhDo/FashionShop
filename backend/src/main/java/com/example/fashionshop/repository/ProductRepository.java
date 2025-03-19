a1102ce38c6b1c2157dd6345bc181b5640cdbc9a
4ntisocialband <4ntisocial@gmail.com>
2025-03-19 10:12:18 +0700

    add repository handle custom query

--- backend/src/main/java/com/example/fashionshop/repository/ProductRepository.java
+++ backend/src/main/java/com/example/fashionshop/repository/ProductRepository.java
@@ -3,9 +3,14 @@ package com.example.fashionshop.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.fashionshop.model.Product;
+import java.util.Locale.Category;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.example.fashionshop.model.Product;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>{
-    
+    Page<Product> findAll(Pageable pageable);
+    Page<Product> findByCategory(Category category, Pageable pageable);
 }
