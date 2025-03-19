3b66e8a21f5e2597a6f965dd54c842b0036dbb24
4ntisocialband <4ntisocial@gmail.com>
2025-03-19 17:36:37 +0700

    add product add, update, delete feature

--- backend/src/main/java/com/example/fashionshop/controller/ProductController.java
+++ backend/src/main/java/com/example/fashionshop/controller/ProductController.java
@@ -3,10 +3,17 @@ package com.example.fashionshop.controller;
 import com.example.fashionshop.model.Product;
 import com.example.fashionshop.service.ProductService;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +23,8 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 import java.util.Locale.Category;
-
+import java.util.List;
+import java.util.Map;
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
@@ -64,5 +72,62 @@ public class ProductController {
             return ResponseEntity.ok(products);
         else
             return ResponseEntity.notFound().build();
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/add")
+    public ResponseEntity<?> addProduct(@RequestBody Product product){
+        try{
+            Product savedProduct = productService.addProduct(product);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedProduct);
+        }catch(RuntimeException e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error saving products");
+        }
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping("/update")
+    public ResponseEntity<?> updateProduct(@RequestBody Product product){
+        try{
+            Product updatedProduct = productService.updateProduct(product);
+            return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
+        }catch(RuntimeException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No product found");
+        }
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteProduct(
+        @PathVariable long id
+    ){
+        try{
+            productService.deleteProduct(id);
+            return ResponseEntity.ok("Product deleted successfully");
+        }catch(RuntimeException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No product found");
+        }
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/add_bulk")
+    public ResponseEntity<?> addProducts(@RequestBody List<Product> products){
+        try{
+            Map<String, List<Product>> response = productService.addProducts(products);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        }catch(RuntimeException e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error saving products");
+        }
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping("/update_bulk")
+    public ResponseEntity<?> updateProducts(@RequestBody List<Product> products){
+        try{
+            Map<String, List<Product>> response = productService.updateProducts(products);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        }catch(RuntimeException e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error updating products");
+        }
     }
 }
--- backend/src/main/java/com/example/fashionshop/service/ProductService.java
+++ backend/src/main/java/com/example/fashionshop/service/ProductService.java
@@ -4,12 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import com.example.fashionshop.model.Product;
 import com.example.fashionshop.repository.ProductRepository;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.Locale.Category;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ProductService {
@@ -30,5 +34,71 @@ public class ProductService {
 
     public Page<Product> getProductByCate(Category category, Pageable pageable){
         return productRepository.findByCategory(category, pageable);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public Product addProduct(Product product){
+        Long productId = product.getId();
+        if(productId == null || productRepository.existsById(productId)){
+            throw new RuntimeException("Error in input, id is existed or null");
+        }
+
+        return productRepository.save(product);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public Product updateProduct(Product product){
+        Long productId = product.getId();
+        if(productId == null || !productRepository.existsById(productId)){
+            throw new RuntimeException("Error in input, id is not exist or null");
+        }
+
+        return productRepository.save(product);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void deleteProduct(Long id){
+        if(!productRepository.existsById(id)){
+            throw new RuntimeException("Product not found with ID: " + id);
+        }
+        productRepository.deleteById(id);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public Map<String, List<Product>> addProducts(List<Product> products){
+        if(products == null || products.isEmpty()){
+            throw new RuntimeException("Product list is empty or null");
+        }  
+
+        Map<Boolean, List<Product>> partitioned = products.stream()
+            .collect(Collectors.partitioningBy(product -> 
+                product.getId() != null && !productRepository.existsById(product.getId()))
+            );
+
+        List<Product> addedProducts = productRepository.saveAll(partitioned.get(true));
+        
+        return Map.of(
+            "updated", addedProducts,
+            "failed", partitioned.get(false)
+        );
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public Map<String, List<Product>> updateProducts(List<Product> products){
+        if(products == null || products.isEmpty()){
+            throw new RuntimeException("Product list is empty or null");
+        }  
+
+        Map<Boolean, List<Product>> partitioned = products.stream()
+            .collect(Collectors.partitioningBy(product -> 
+                product.getId() != null && productRepository.existsById(product.getId()))
+            );
+
+        List<Product> updatedProducts = productRepository.saveAll(partitioned.get(true));
+        
+        return Map.of(
+            "updated", updatedProducts,
+            "failed", partitioned.get(false)
+        );
     }
 }
