44ad8fb2211aea335beb7936e44f52a63af887f2
4ntisocialband <4ntisocial@gmail.com>
2025-03-19 10:10:40 +0700

    fix import

--- backend/src/main/java/com/example/fashionshop/controller/ProductController.java
+++ backend/src/main/java/com/example/fashionshop/controller/ProductController.java
@@ -13,11 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.Locale.Category;
-import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api/products")
