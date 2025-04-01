21b26c960a2807a0587820086ad88d4e290a146f
DuongDang28 <dangduonggg05@gmail.com>
2025-04-01 19:14:17 +0700

    Update App.js
    thêm Routes cho trang home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,20 +10,24 @@ import Orders from './pages/Orders';
 import Users from './pages/Users';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Home from './pages/Home';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Routes>
+        <Route path="/home" element={<Home />} />
+      <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/" element={<Layout>
-          <Routes>
+      <Routes>
             <Route index element={<Dashboard />} />
             <Route path="/products" element={<Products />} />
             <Route path="/orders" element={<Orders />} />
             <Route path="/users" element={<Users />} />
+            </Routes>
           </Routes>
         </Layout>} />
       </Routes>
